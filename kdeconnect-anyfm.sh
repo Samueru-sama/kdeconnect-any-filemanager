ac16fc53e6426134278327b1f886cbc3edb02463
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2024-05-04 06:28:06 -0400

    Make the script determine which qdbus to use

--- kdeconnect-anyfm.sh
+++ kdeconnect-anyfm.sh
@@ -1,27 +1,35 @@
 #!/bin/sh
 
-# Safety checks
-pgrep kdeconnect > /dev/null || /usr/lib/kdeconnectd || { echo "Error kdeconnect, is it installed?"; exit 1; }
-mount | grep kdeconnect && { echo "Phone is already mounted" & exit 1; }
+PHONEDIR="${XDG_DATA_HOME:-$HOME/.local/share}/Phone" # Replace this with where you want it to be
 
-PHONEID=$(kdeconnect-cli -a --id-name-only 2>/dev/null | awk '{print $1}')
-if [ -z "$PHONEID" ]; then 
-    echo "No Phone Connected"
-    exit 1
+# CHECK IF PHONE IS ALREADY MOUNTED
+mount | grep kdeconnect && { echo "Phone is already mounted"; exit 0; }
+
+# CHECK FOR QDBUS
+if command -v qdbus6 >/dev/null 2>&1; then
+    QDBUS=qdbus6
+elif command -v qdbus-qt5 >/dev/null 2>&1; then
+    QDBUS=qdbus-qt5
+else
+    echo "You need qdbus for this script to work"; exit 1
 fi
 
 # Mount phone
-qdbus6 org.kde.kdeconnect /modules/kdeconnect/devices/"$PHONEID"/sftp mountAndWait || { echo "Error mounting"; exit 1; }
+PHONEID=$(kdeconnect-cli -a --id-name-only 2>/dev/null | awk '{print $1}')
+if [ -z "$PHONEID" ]; then 
+	echo "No phone connected"; exit 1
+fi
+
+"$QDBUS" org.kde.kdeconnect /modules/kdeconnect/devices/"$PHONEID"/sftp mountAndWait || { echo "Error mounting"; exit 1; }
 
 # Get paths
-PHONEPATHS=$(qdbus6 org.kde.kdeconnect /modules/kdeconnect/devices/"$PHONEID"/sftp getDirectories 2>/dev/null)
-PHONEPATH1=$(echo "$PHONEPATHS" | awk 'NR==1 {print $1}' | sed 's/://g' )
-PHONEPATH2=$(echo "$PHONEPATHS" | awk 'NR==2 {print $1}' | sed 's/://g' )
-DIRNAME1=$(echo "$PHONEPATHS" | awk 'NR==1 {print $2}' )
-DIRNAME2=$(echo "$PHONEPATHS" | awk 'NR==2 {print $2}' )
+PHONEPATHS=$("$QDBUS" org.kde.kdeconnect /modules/kdeconnect/devices/"$PHONEID"/sftp getDirectories 2>/dev/null)
+PHONEPATH1=$(echo "$PHONEPATHS" | awk -F ":" 'NR==1 {print $1}')
+PHONEPATH2=$(echo "$PHONEPATHS" | awk -F ":" 'NR==2 {print $1}')
+DIRNAME1=$(echo "$PHONEPATHS" | awk -F ":" 'NR==1 {print $2}')
+DIRNAME2=$(echo "$PHONEPATHS" | awk -F ":" 'NR==2 {print $2}')
 
 # Make and link dirs
-PHONEDIR="$HOME/.local/Phone"
 mkdir -p "$PHONEDIR"
 ln -s "$PHONEPATH1" "$PHONEDIR/$DIRNAME1" 2>/dev/null
 ln -s "$PHONEPATH2" "$PHONEDIR/$DIRNAME2" 2>/dev/null
